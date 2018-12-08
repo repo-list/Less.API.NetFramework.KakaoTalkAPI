5d84b4ba562e699bcc8ddbb6b54e4274c2f1a2ac
Eric Kim <lessbdr@hotmail.com>
2018-12-08 22:20:52 +0900

    Deleted unnecessary files

--- ClipboardManagerAPI/ClipboardManager.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Drawing;
-using System;
-using System.Runtime.InteropServices;
-using Less.API.NetFramework.WindowsAPI;
-
-// .Net Framework에서 기본 제공하는 Clipboard 클래스는 불안정하기 때문에, 전부 Native API로 처리해야 함.
